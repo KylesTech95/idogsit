d0593844c57850bb232f4eaba6add29b71786943
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-08-13 15:36:14 -0400

    feat: Package.json - easily backup/migrate data with scripts

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,25 +1,50 @@
 require('dotenv').config()
 const mysql = require('mysql2/promise');
-// db object
-
-
-const pool = mysql.createPool({
+const dump = require('mysqldump')
+const dumpToFile = require('path').resolve(__dirname,'dogsit.sql')
+// connection
+const connection = {
     host:process.env.MYSQL_H,
     user:process.env.MYSQL_U,
     password:process.env.MYSQL_P,
     database:process.env.MYSQL_D,
     port:process.env.MYSQL_PO
-})
-
-const exitProcess = (code) => require('process').exit(code);
-
-
-
-
-// functions
-const testConnection = async pool => {try { console.log([...await pool.query('select * from pets')][0]); exitProcess(0) } catch (err) { throw new Error(err); }
 }
-// testConnection(pool)
+// pool the database
+const pool = mysql.createPool(connection)
+const exitProcess = (code) => require('process').exit(code); // exit process 1
+
+
+const testConnection = async pool => {
+    try { console.log([...await pool.query('select * from pets')][0]); exitProcess(0) } catch (err) { throw new Error(err); }
+}
+
+
+
+/* ----------------------------------- Dump Mysql ----------------------------------- */
+// dump mysql database to .sql file
+// function dumpMysqlToLib(connection,filepath){
+//     // dump file
+//     dump({
+//         connection,
+//         filepath
+//     })
+//     .then((data)=>{
+//         console.log(data)
+//         const dataString = JSON.stringify(data)
+//         console.log('Database dumped successfully to ' + filepath);
+//     })
+//     .catch((err=>{
+//         return err ? console.error('Error Dumping daabse:',err) : null;
+//     }))
+// }
+// if(process.argv[process.argv.length-1] === 'dump'){
+//     dumpMysqlToLib(connection,dumpToFile) // dumpToFile property should get passed as a varibale to match the object's [dump] property
+// }
+/* ----------------------------------- Dump Mysql ----------------------------------- */
+
+
+
 module.exports = {pool, testConnection}
 
 
--- server/lib/dogsit.sql
+++ server/lib/dogsit.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 9.3.0, for macos15.2 (arm64)
+-- MySQL dump 10.13  Distrib 9.1.0, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dogsit
 -- ------------------------------------------------------
--- Server version	9.3.0
+-- Server version	8.0.43
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -109,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-13 13:54:49
+-- Dump completed on 2025-08-13 15:27:30
